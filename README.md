d0baf4f86557248277c1e97ab305853d17ef9f07
Brewen Couaran <45310490+brewcoua@users.noreply.github.com>
2024-01-19 14:29:42 +0100

    feat: move home to home/ and add readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# github.com/brewcoua/dotfiles
+Brewen Couaran's dotfiles, forked from [twpayne/dotfiles](https://github.com/twpayne/dotfiles), managed with [chezmoi](https://github.com/twpayne/chezmoi).
+
+## Installing
+```bash
+curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/brewcoua/dotfiles/master/install.sh | sh
+```
---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e # -e: exit on error
+
+if [ ! "$(command -v chezmoi)" ]; then
+	BIN_DIR="$HOME/.local/bin"
+	CHEZMOI="$BIN_DIR/chezmoi"
+	if [ $(command -v curl)" ]; then
+		sh -c "$(curl -fsSL https://get.chezmoi.io)" -- -b "$BIN_DIR"
+	elif [ "$(command -v wget)" ]; then
+		sh -c "$(wget -qO- https://get.chezmoi.io)" -- -b "$BIN_DIR"
+	else
+		echo "You need to have either curl or wget installed." >&2
+		exit 1
+	fi
+else
+	CHEZMOI="chezmoi"
+fi
+
+SCRIPT_DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+
+# Replace current process with chezmoi init
+exec "$CHEZMOI" init --apply "--source=$SCRIPT_DIR"
